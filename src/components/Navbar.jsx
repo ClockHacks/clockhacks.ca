559acbcbaf028659c8046bccba9d100b3743fba3
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-08-17 11:24:26 -0400

    add burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,27 @@ const Navbar = () => {
     
     const pages = ['About', 'Contact', 'FAQ', 'Schedule', 'Speakers', 'Sponsors', 'Team']
 
+    function HandleClick() {
+        var x = document.getElementById("nav");
+        if (x.className === "nav") {
+          x.className += " responsive";
+        } else {
+          x.className = "nav";
+        }      
+    }
+
 	return (
-	<div className = 'nav'>
-		logo
-        
-        <div>
+    <div className = 'nav' id = 'nav'>
+        <a style = {{marginLeft: '0px'}}>
+            logo
+        </a>
         {
             pages.map(page => (
                 <a href={`#${page.toLowerCase()}`}>{page} </a>
             ))
         }
-        </div>
+
+        <span class="material-icons menuicon" onClick = {HandleClick}>menu</span>
 
 	</div>
 	)
