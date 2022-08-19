226c834061896d190d8d371bcf27795995521488
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-08-19 19:41:14 -0400

    install and use AOS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,28 @@
 import Collaboration from '../assets/collaboration.svg'
 
 const About = () => {
+
 	return (
-			<div id="about" className="container spaced">
+		<div id="about" className="container spaced" data-aos = 'fade-up' data-aos-duration = '1200'>
 
-				<div>
-				<h1>
-					By <gradient> students,</gradient> for <gradient> students</gradient>
-				</h1>
-				<p>
-					ClockHacks is a hackathon created by a group of high school students from Waterloo, Ontario.
-					<br></br><br></br>
-					Our mission is to make hackathons accessible and fun for high school students around the world. Themed around time, Clockhacks will take place online on DATE.
-					<br></br><br></br>
-					You will be able to work in teams of up to 4 to create a website, game, app, robot, or whatever tech-related product you want. Participate in activites, speeches, workshops, and events to learn more about the world of tech. At the end of the event, pitch your project to a panel of judges and compete for prizes. 
-					<br></br><br></br>
-					Join hackers from across North America this November for a weekend of fun and innovation at ClockHacks! 
-				</p>
-				</div>
-
-				<img src = {Collaboration} alt = 'collabimg' className = 'img'></img>
-
+			<div>
+			<h1>
+				By <gradient> students,</gradient> for <gradient> students</gradient>
+			</h1>
+			<p>
+				ClockHacks is a hackathon created by a group of high school students from Waterloo, Ontario.
+				<br></br><br></br>
+				Our mission is to make hackathons accessible and fun for high school students around the world. Themed around time, Clockhacks will take place online on DATE.
+				<br></br><br></br>
+				You will be able to work in teams of up to 4 to create a website, game, app, robot, or whatever tech-related product you want. Participate in activites, speeches, workshops, and events to learn more about the world of tech. At the end of the event, pitch your project to a panel of judges and compete for prizes. 
+				<br></br><br></br>
+				Join hackers from across North America this November for a weekend of fun and innovation at ClockHacks! 
+			</p>
 			</div>
+
+			<img src = {Collaboration} alt = 'collabimg' className = 'img'></img>
+
+		</div>
 	)
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,13 @@ const Home = () => {
     <div className = 'container' id = 'home' style = {{paddingTop: '5vh'}}>
         <img src = {logo} alt = 'logo' className='centered' id = 'homelogo'></img>
         <h1 id = 'title'>
-            ClockHacks
+            <gradient>ClockHacks</gradient>
         </h1>
         <p style = {{textAlign: 'center', fontSize: 'max(2vw, 20px)'}}>
-            A hackathon for 
-            <gradient> time </gradient>
+            A hackathon for time | November 4 - 6, 2022
             <br></br> 
-            <button>Applications Open Soon</button>
+            <button>Applications Open Soon</button>{' '}
+            <button>Sponsor Us</button>
         </p>
     </div>
   )
