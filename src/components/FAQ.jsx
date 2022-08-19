44f00d26f9c566451869afe0e7ff5cb36e80cd40
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-08-19 16:59:43 -0400

    FAQ styling

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,11 +1,63 @@
 const FAQ = () => {
 	return (
 		<div id="faq" className = 'container'>
-			<h1>Frequently Asked Questions</h1>
-			<p>
-				When is ClockHacks?
-				idk lol
-			</p>
+			<h1 style = {{textAlign: 'center'}}>Frequently Asked Questions</h1>
+
+			<div className = 'faqdiv'>
+
+				<div>
+
+				<h1 className = 'header'>Before the Hackathon</h1>
+
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+				<details>
+					<summary>Who can participate?</summary>
+					<p>Students of any age from any part of the world are welcome to participate in ClockHacks. We are aiming for this to be a hackathon targeted toward high school students.</p>
+				</details>
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+
+				</div>
+
+				<div className = 'faqcolumn'>
+
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+				<details>
+					<summary>What is a hackathon?</summary>
+					<p>A hackathon is an event where designers, programmers, engineers, and tech enthusiasts gather to create something within a given time limit.</p>
+				</details>
+
+				</div>
+
+			</div>
 		</div>
 	)
 }
