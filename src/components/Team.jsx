a20c1dc22bca31cfe98f7d62b89d44b9c7e5edc3
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-08-22 12:26:15 -0400

    team page (it kinda sucks but whatever)

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,7 +1,55 @@
+import jay from '../assets/team/jay.jpg'
+import jeffrey from '../assets/team/jeffrey.jpg'
+import sean from '../assets/team/sean.jpg'
+import cera from '../assets/team/cera.jpg'
+
 const Sponsors = () => {
 	return (
-		<div id="team">
-			This is the team page
+		<div id="team" className = 'container'>
+			<h1 style = {{textAlign: 'center'}}>
+				Our Team
+				<p>The amazing people behind it all</p>
+			</h1>
+
+			<div id = 'teamcontainer'>
+			<div className = 'column'>
+				<a href = 'https://www.linkedin.com/in/pilotprix/' target = '_blank' rel = 'noreferrer'>
+				<img src = {sean} alt = 'headshot'></img>
+				<h1>
+					Sean Wang
+					<p>Co-founder, financing and logistics lead</p>
+				</h1>
+				</a>
+
+				<a href = 'https://www.linkedin.com/in/jeffrey-zang-49b872246/' target = '_blank' rel = 'noreferrer'> 
+					<img src = {jeffrey} alt = 'headshot'></img>
+					<h1>
+						Jeffrey Zang
+						<p>Co-founder, technical co-lead</p>
+					</h1>
+				</a>
+
+			</div>
+
+			<div className = 'column'>
+				<a href = 'https://www.linkedin.com/in/jay-yuan-ren-107892211/' target = '_blank' rel = 'noreferrer'>
+					<img src = {jay} alt = 'headshot'></img>
+					<h1>
+						Jay Ren
+						<p>Co-founder, technical co-lead</p>
+					</h1>
+				</a>
+
+				<a href = 'https://www.linkedin.com/in/cera-wang-171699240/' target = '_blank' rel = 'noreferrer'>
+					<img src = {cera} alt = 'headshot'></img>
+					<h1>
+						Cera Wang
+						<p>Co-founder, design lead</p>
+					</h1>
+				</a>
+			</div>
+			</div>
+
 		</div>
 	)
 }
