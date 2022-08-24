ec19669a6a99a7b6f21c9222a66202cfcca96b5c
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-08-24 12:37:58 -0400

    make the footer better

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,22 +6,26 @@ const Footer = () => {
 		
 		<img src = {banner} alt = 'banner' style = {{width: '100%'}}></img>
 		<div className = 'spaced' id = 'footerspaced'>
-			<h1>
+			<p>
 			Copyright © 2022 ClockHacks
-			</h1>
-			<h1>
+			</p>
+
+			<p>
       
 			<a target = '_blank' rel = 'noreferrer' onClick = {
-        () => {
-          window.scrollTo(0,0)
-        }
-      }>Back to Top</a>
+				() => {
+				window.scrollTo(0,0)
+				}
+			}>Back to Top</a>
 			<a href = 'https://static.mlh.io/docs/mlh-code-of-conduct.pdf' target = '_blank' rel = 'noreferrer'>MLH Code of Conduct</a>
 			<a href = 'https://github.com/ClockHacks/clockhacks-website/blob/main/LICENSE' target = '_blank' rel = 'noreferrer'>License</a>
+			</p>
+			
+			<p>
 			<a href = 'https://discord.gg/UKjQBdy26S' target = '_blank'><i class = 'bi bi-discord'></i></a>
-      <a href = 'https://github.com/ClockHacks' target = '_blank'><i class = 'bi bi-github'></i></a>
-      <a target = '_blank'><i class = 'bi bi-linkedin'></i></a>
-			</h1>
+			<a href = 'https://github.com/ClockHacks' target = '_blank'><i class = 'bi bi-github'></i></a>
+			<a target = '_blank'><i class = 'bi bi-linkedin'></i></a>
+			</p>
 		</div>
 
 		</footer>
