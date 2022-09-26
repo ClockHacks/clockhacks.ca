0af7e95489860ddd9ef5a716747dc62c47c0fe1d
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-09-26 19:42:54 -0400

    make schedule a bit better but not actually

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,17 +1,50 @@
 import gear5 from '../assets/images/gear3.png'
 
+const nov4 = [
+	{
+		'time': '7:00 PM',
+		'name': 'Hacking Starts!'
+	}
+]
+
+// nov5 = [
+// 	{
+// 		'time': '7:00 PM',
+// 		'name': 'Hacking Starts!'
+// 	}
+// ]
+
+const nov6 = [
+	{
+		'time': '11:00 AM',
+		'name': 'Hacking Ends!'
+	}
+]
+
 const Schedule = () => {
 	return (
 		<div id="schedule" className = 'container' style = {{textAlign: 'center'}}>
 			<img loading = 'lazy' src = {gear5} alt = 'gear5' id = 'gear5'></img>
 			<h1>Schedule</h1>
-			<p>
-				November 4
-				7:00 PM - Hacking Starts!
-				<br></br>
-				November 6
-				11:00 AM - Hacking Ends!
-			</p>
+
+			{/* <div id = 'nov4'>
+				{nov4.map(
+					(i, index) => (
+						<>
+						<div className='left'>
+							{i.time}
+						</div>
+						<div className='right'>
+							{i.name}
+						</div>						
+						</>
+					)
+				)
+				}
+			</div> */}
+			November 4 | 7:00 PM | Hacking Starts!<br/>
+			November 6 | 11:00 AM | Hacking Ends!
+
 		</div>
 	)
 }
