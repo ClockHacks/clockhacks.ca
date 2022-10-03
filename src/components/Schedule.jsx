77354ea7b9e81f1867c1e5870694bf475801b284
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-10-03 17:05:34 -0400

    build

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -31,7 +31,7 @@ const Schedule = () => {
 			{/* <img loading = 'lazy' src = {gear5} alt = 'gear5' id = 'gear5'></img> */}
 			<h1>Schedule</h1>
 
-			<div id = 'nov4'>
+			{/* <div id = 'nov4'>
 				{nov4.map(
 					(i, index) => (
 						<div className = 'wrapper'>
@@ -45,9 +45,9 @@ const Schedule = () => {
 					)
 				)
 				}
-			</div>
-			{/* November 4 | 7:00 PM | Hacking Starts!<br/>
-			November 6 | 11:00 AM | Hacking Ends! */}
+			</div> */}
+			November 4 | 7:00 PM | Hacking Starts!<br/>
+			November 6 | 11:00 AM | Hacking Ends!
 
 		</div>
 	)
