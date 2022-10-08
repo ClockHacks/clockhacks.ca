e7f3b3dc108ce45b30b702f81c49370c30299822
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-10-08 18:11:14 -0400

    make footer look better

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,56 @@
 import banner from '../assets/banner.jpg'
 import gear3 from '../assets/images/gear3.png'
+import logo from '../assets/Logo.png'
 
 const Footer = () => {
   return (
     <footer>
 		
 		<img loading = 'lazy' src = {banner} alt = 'banner' style = {{width: '100%'}}></img>
-		<div className = 'spaced' id = 'footerspaced'>
-			<img loading = 'lazy' src = {gear3} alt = 'gear3' id = 'gear7'></img>
-			<p>
-			Copyright © 2022 ClockHacks
-			</p>
-
-			<p>
-      
-			<a target = '_blank' rel = 'noreferrer' onClick = {
-				() => {
-				window.scrollTo(0,0)
-				}
-			}>Back to Top</a>
-			<a href = 'https://static.mlh.io/docs/mlh-code-of-conduct.pdf' target = '_blank' rel = 'noreferrer'>MLH Code of Conduct</a>
-			<a href = 'https://github.com/ClockHacks/clockhacks-website/blob/main/LICENSE' target = '_blank' rel = 'noreferrer'>License</a>
-			</p>
+		<div id = 'footer'>
+			{/* <img loading = 'lazy' src = {gear3} alt = 'gear3' id = 'gear7'></img> */}
 			
-			<p>
-			<a href = 'https://discord.gg/UKjQBdy26S' target = '_blank'><i class = 'bi bi-discord'></i></a>
-			<a href = 'https://github.com/ClockHacks' target = '_blank'><i class = 'bi bi-github'></i></a>
-			<a href = 'https://www.instagram.com/clockhacks/' target = '_blank'><i class = 'bi bi-instagram'></i> </a>
-			<a href = 'https://www.linkedin.com/company/clockhacks/' target = '_blank'><i class = 'bi bi-linkedin'></i></a>
-			</p>
+			<div id = 'copyright'>
+				<h1>
+					<img src = {logo} alt = 'footerlogo' id = 'footerlogo'></img>
+					<gradient>ClockHacks</gradient>
+				</h1>
+				<p>Copyright © 2022 ClockHacks</p>
+				<p id='footersocials'>
+					<a href = 'https://discord.gg/UKjQBdy26S' target = '_blank'><i class = 'bi bi-discord'></i></a>
+					<a href = 'https://github.com/ClockHacks' target = '_blank'><i class = 'bi bi-github'></i></a>
+					<a href = 'https://www.linkedin.com/company/clockhacks/' target = '_blank'><i class = 'bi bi-linkedin'></i></a>
+					<a href = 'https://www.instagram.com/clockhacks/' target = '_blank'><i class = 'bi bi-instagram'></i> </a>
+				</p>
+			</div>
+
+			<div>
+				<h2>NAVIGATION</h2>
+				<a href = '#about'>About</a>
+				<a href = '#clock'>Countdown</a>
+				<a href = '#schedule'>Schedule</a>
+				<a href = '#sponsors'>Sponsors</a>
+				<a href = '#team'>Team</a>
+				<a href = '#contact'>Contact</a>
+			</div>
+
+			<div>
+				<h2>LEGAL</h2>
+				<a target = '_blank' rel = 'noreferrer' onClick = {
+					() => {
+						window.scrollTo(0,0)
+					}
+				}>Back to Top</a>
+				<a href = 'https://github.com/ClockHacks/clockhacks-website/blob/main/LICENSE' target = '_blank' rel = 'noreferrer'>License</a>
+				<a href = 'https://mlh.io/' target = '_blank' rel = 'noreferrer'>Major League Hacking</a>
+				<a href = 'https://static.mlh.io/docs/mlh-code-of-conduct.pdf' target = '_blank' rel = 'noreferrer'>MLH Code of Conduct</a>
+				<a href = 'https://drive.google.com/file/d/1yl3QcwDGCNNPabv4qlJwmaHEyJbUhIhg/view' target = '_blank' rel = 'noreferrer'>Sponsorship Package</a>
+			</div>
+
+
 		</div>
 
-		</footer>
-
+	</footer>
   )
 }
 
